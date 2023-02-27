c0ff963683460dd4e08dc702c134bfe94912e810
Anurag <rawatanurag362@gmail.com>
2023-02-27 11:14:41 +0530

    First Commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../Components/navBar";
+
+import "./Home.css";
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+
+    </div>
+  );
+}
