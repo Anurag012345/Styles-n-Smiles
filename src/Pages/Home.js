432c362f382986adbdfcb3e3bc4d4e94fb3f8808
Anurag <rawatanurag362@gmail.com>
2023-02-28 12:27:09 +0530

    Second Commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,23 @@
 import React from "react";
 import NavBar from "../Components/navBar";
+import ImageSlider from "../Components/ImageSlider"
+import img1 from "../Images/img1.png"
+import img2 from "../Images/img2.png"
+import img3 from "../Images/img3.png"
+
 
 import "./Home.css";
 export default function Home() {
   return (
     <div>
       <NavBar />
-
+      <div>
+        <ImageSlider infinite timer={5000} stopOnManual>
+          <img src={img1} alt="" />
+          <img src={img2} alt="" />
+          <img src={img3} alt="" />
+        </ImageSlider>
+      </div>
     </div>
   );
 }
